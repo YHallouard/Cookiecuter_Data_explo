b6b81f32c2d84f38a39bde78e5b864430e50da13
Yann HALLOUARD <yann.hal@gmail.com>
2020-12-01 14:51:10 +0100

    requirement & appnope

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
+from os import path
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-required = ['matplotlib',
-            'numpy',
-            'pandas',
-            'notebook',
-            'jupyter'
-            ]
+required_setup = ['pytest-runner',
+                  'wheel',
+                  'flake8']
+required_tests = ['pytest',
+                  'pytest-cov',
+                  'treon']
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+required_install = [x.strip() for x in all_reqs]
 __version__ = 'init'
 exec(open('{{ cookiecutter.repo_name }}/version.py').read())
 
@@ -23,7 +29,7 @@ setup(
     long_description_content_type="text/markdown",
     setup_requires=['pytest-runner', 'wheel', 'flake8'],
     tests_require=['pytest', 'pytest-cov', 'treon'],
-    install_requires=required,
+    install_requires=all_reqs,
     license='{% if cookiecutter.open_source_license == "MIT" %}MIT\
     {% elif cookiecutter.open_source_license == "BSD-3-Clause" %}BSD-3{% endif %}',
     classifiers=[
